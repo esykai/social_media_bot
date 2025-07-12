b1bc36ef605c58e65bf06469f44d1384e132fae3
Andrey Abramov <45533799+esykai@users.noreply.github.com>
2025-07-12 16:28:36 +0300

    Выходим из облачного сервера Bot Api

--- main.py
+++ main.py
@@ -14,13 +14,11 @@ from config import TELEGRAM_BOT_TOKEN, TEMP_FOLDER, MAX_MEDIA_FILES, MAX_TEXT_LE
 from social_poster import post_to_telegram, post_to_x
 
 session = AiohttpSession(
+    token=TELEGRAM_BOT_TOKEN,
     api=TelegramAPIServer.from_base('http://telegram-bot-api:8081/bot'),
     timeout=60,
 )
-bot = Bot(
-    token=TELEGRAM_BOT_TOKEN,
-    session=session
-)
+bot = Bot(session=session)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
