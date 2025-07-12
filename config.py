26eb12e36cb6e4484f6ca69a7d44f346d7156687
Andrey Abramov <45533799+esykai@users.noreply.github.com>
2025-07-12 14:14:07 +0300

    Делаю ci/cd процесс

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_GROUP_ID = os.getenv("TELEGRAM_GROUP_ID")
+
+X_API_KEY = os.getenv("X_API_KEY")
+X_API_SECRET = os.getenv("X_API_SECRET")
+X_ACCESS_TOKEN = os.getenv("X_ACCESS_TOKEN")
+X_ACCESS_TOKEN_SECRET = os.getenv("X_ACCESS_TOKEN_SECRET")
+
+TEMP_FOLDER = os.getenv("TEMP_FOLDER", "temp")
+ALLOWED_USER_ID = int(os.getenv("ALLOWED_USER_ID", "0"))
+MAX_MEDIA_FILES = int(os.getenv("MAX_MEDIA_FILES", "10"))
+MAX_TEXT_LENGTH = int(os.getenv("MAX_TEXT_LENGTH", "2000"))
